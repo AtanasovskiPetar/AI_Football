ae58f86bb759f363079763569294de50289c39af
AtanasovskiPetar <patanasovski25@gmail.com>
2023-12-20 12:11:31 +0100

    Docker Deployment

--- AI_Football/AIFootball/test_models.py
+++ AI_Football/AIFootball/test_models.py
@@ -1,19 +1,20 @@
 import gymnasium as gym
 import gym_envs.envs.ai_football
-from stable_baselines3 import DDPG
+from stable_baselines3 import PPO
 import pygame
 
 pygame.init()
 pygame.display.set_mode((1366, 768), pygame.RESIZABLE)
 
-env = gym.make("AiFootball", render_mode='human')
+env = gym.make("AiFootball-v0", render_mode='human')
 env.reset()
-model = DDPG.load("models/ai_football_3/900000.zip", env=env)
+model = PPO.load("models/PPO/ai_football_1/4000k", env=env)
 
 env = model.get_env()
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
+    print(f'Rew: {rewards}')
     print(f'Action: {action}')
     env.render()
--- AI_Football/AIFootball/train.py
+++ AI_Football/AIFootball/train.py
@@ -1,23 +1,22 @@
 import numpy as np
 import gymnasium as gym
 import gym_envs.envs.ai_football
-from stable_baselines3 import DDPG
+from stable_baselines3 import PPO
 from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.env_util import make_vec_env
+
+# vec_env = make_vec_env('AiFootball-v0', n_envs=4)
+# vec_env.reset()
 
 env = gym.make("AiFootball-v0")
 env.reset()
-# The noise objects for DDPG
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 # Model initialization
-# model = DDPG("MultiInputPolicy", env, action_noise=action_noise, verbose=1, tensorboard_log="logs")
+model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="PPO_logs", ent_coef=0.1)
+# model = DDPG.load("models/ai_football_3/900000.zip", env=env, action_noise=action_noise)
 
-model = DDPG.load("models/ai_football_3/900000.zip", env=env, action_noise=action_noise)
-
-TIMESTAMPS = 10000
-i = 91
+TIMESTAMPS = 100000
+i = 1
 while True:
-    model.learn(total_timesteps=TIMESTAMPS, reset_num_timesteps=False, tb_log_name="ai_football_7")
-    if i % 5 == 0:
-        model.save(f'models/ai_football_3/{TIMESTAMPS * i}')
+    model.learn(total_timesteps=TIMESTAMPS, reset_num_timesteps=False, tb_log_name="ai_football_1")
+    model.save(f'models/PPO/ai_football_1/{i}00k')
     i += 1
